429013dc8fbdba8ee5fe42622808c96ca43f8e15
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-04-11 08:39:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/yourstory/view/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yourstory.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yourstory.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
