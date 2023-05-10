f9cee10f8540cf52162f915a2bf653bc26c773dc
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-05-11 02:32:31 +0700

    update

--- app/src/test/java/com/example/yourstory/ui/story/StoryViewModelTest.kt
+++ app/src/test/java/com/example/yourstory/ui/story/StoryViewModelTest.kt
@@ -1,7 +1,6 @@
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.*
 import androidx.paging.*
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListUpdateCallback
 import com.example.yourstory.model.StoryRequest
 import com.example.yourstory.model.StoryResponseData
@@ -21,39 +20,14 @@ import org.mockito.Mock
 import org.mockito.Mockito.doReturn
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Response
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
-
 
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class StoryViewModelTest {
 
-//    companion object {
-//        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<StoryResponseData>() {
-//            override fun areItemsTheSame(
-//                oldItem: StoryResponseData,
-//                newItem: StoryResponseData
-//            ): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun areContentsTheSame(
-//                oldItem: StoryResponseData,
-//                newItem: StoryResponseData
-//            ): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//        }
-//    }
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
-
-
-//    @get:Rule
-//    val mainDispatcherRules = MainDispatcherRule()
 
     @Mock
     private lateinit var repository: Repository
@@ -62,46 +36,27 @@ class StoryViewModelTest {
 
     private lateinit var viewModel: StoryViewModel
 
-//    private val lifecycleOwner = FakeLifecycleOwner()
-
-    private val testDispatcher = TestCoroutineDispatcher()
-    private val testScope = createTestCoroutineScope(testDispatcher)
+    private val testDispatcher = UnconfinedTestDispatcher()
 
     private val dummyStoriesResponse = DataDummy.generateDummyStories()
 
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-
         viewModel = StoryViewModel(repository, sessionManager)
     }
 
     @After
     fun tearDown() {
         Dispatchers.resetMain()
-        testScope.cleanupTestCoroutines()
     }
-
 
     @Test
     fun `when getStories Should Not Null and Return Success`() = runTest {
 
-        val testCoroutineScope = TestCoroutineScope()
-
-//        val data: PagingData<StoryResponseData> = StoryPagingSource.snapshot(dummyStoriesResponse.listStory)
-//        val expectedStories = MutableLiveData<PagingData<StoryResponseData>>()
-//        expectedStories.value = data
-
-//        val response = Response.success(dummyStoriesResponse)
-//
-//        val token = viewModel.getToken() ?: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLTZkbUJHMFh1Wlk1UU5NdlkiLCJpYXQiOjE2ODM0Nzc5NzV9.MZPnd9v9Cpipg5HbMr9CkeA1UZENHQ-jq8LugXSmDiM"
-////        Mockito.`when`(repository.getStories(token)).thenReturn(expectedStories)
-//
-//        val listStoryViewModel = StoryViewModel(repository, sessionManager, testCoroutineScope)
-//        var actualStories: Flow<PagingData<StoryResponseData>>? = null
+        val testCoroutineScope = TestScope()
 
         testCoroutineScope.launch(Dispatchers.IO) {
-//            doReturn(response).`when`(repository).getAllStories(token, null, null, null)
             viewModel.storyPagingFlow.collectLatest {
 
                 val differ = AsyncPagingDataDiffer(
@@ -109,44 +64,25 @@ class StoryViewModelTest {
                     updateCallback = noopListUpdateCallback,
                     workerDispatcher = Dispatchers.Main,
                 )
-                differ.submitData(it)
 
                 Assert.assertNotNull(it)
+
+                differ.submitData(it)
+
                 Assert.assertNotNull(differ.snapshot())
                 Assert.assertEquals(dummyStoriesResponse.listStory, differ.snapshot())
                 Assert.assertEquals(dummyStoriesResponse.listStory.size, differ.snapshot().size)
                 Assert.assertEquals(dummyStoriesResponse.listStory[0].id, differ.snapshot()[0]?.id)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].name, differ.snapshot()[0]?.name)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].description, differ.snapshot()[0]?.description)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].photoUrl, differ.snapshot()[0]?.photoUrl)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].createdAt, differ.snapshot()[0]?.createdAt)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].lat, differ.snapshot()[0]?.lat)
+                Assert.assertEquals(dummyStoriesResponse.listStory[0].lon, differ.snapshot()[0]?.lon)
+
             }
-//            actualStories = listStoryViewModel.storyPagingFlow
-//
-//            actualStories?.collectLatest {
-//                println("test - actualStories: ${it}")
-//                println("test - expectedStories: ${expectedStories.value}")
-//
-//                Assert.assertNotNull(it)
-//                Assert.assertEquals(expectedStories.value, it)
-//            }
         }
 
-//        println("test - actualStories: ${actualStories}")
-//
-//        val differ = AsyncPagingDataDiffer(
-//            diffCallback = StoryAdapter.DIFF_CALLBACK,
-//            updateCallback = noopListUpdateCallback,
-//            workerDispatcher = Dispatchers.Main,
-//        )
-
-//        submitDataAndWaitForSnapshot(differ, actualStories)
-//
-//        println("test - actualStories: ${differ.snapshot().items}")
-//        println("test - actualStories: ${differ.snapshot().items.size}")
-//        println("test - expectedStories: ${expectedStories.value}")
-//
-//
-//        Assert.assertNotNull(differ.snapshot())
-//        Assert.assertEquals(dummyStoriesResponse.listStory, differ.snapshot())
-//        Assert.assertEquals(dummyStoriesResponse.listStory.size, differ.snapshot().size)
-//        Assert.assertEquals(dummyStoriesResponse.listStory[0].id, differ.snapshot()[0]?.id)
     }
 
     @Test
@@ -189,165 +125,9 @@ class StoryViewModelTest {
 
 }
 
-
-
-
-//suspend fun submitDataAndWaitForSnapshot(
-//    differ: AsyncPagingDataDiffer<StoryResponseData>,
-//    data: PagingData<StoryResponseData>
-//) {
-//    val latch = CountDownLatch(1)
-//    differ.addLoadStateListener { loadStates ->
-//        if (loadStates.refresh is LoadState.NotLoading) {
-//            latch.countDown()
-//        }
-//    }
-//    differ.submitData(data)
-//    withContext(Dispatchers.IO) {
-//        latch.await(5, TimeUnit.SECONDS)
-//    }
-//}
-
-
-fun <T> LiveData<T>.getOrAwaitValue(
-    time: Long = 2,
-    timeUnit: TimeUnit = TimeUnit.SECONDS,
-    afterObserve: () -> Unit = {}
-): T {
-    var data: T? = null
-    val latch = CountDownLatch(1)
-    val observer = object : Observer<T> {
-        override fun onChanged(o: T) {
-            data = o
-            latch.countDown()
-            this@getOrAwaitValue.removeObserver(this)
-        }
-    }
-    this.observeForever(observer)
-
-    try {
-        afterObserve.invoke()
-
-        // Don't wait indefinitely if the LiveData is not set.
-        if (!latch.await(time, timeUnit)) {
-            throw TimeoutException("LiveData value was never set.")
-        }
-    } finally {
-        this.removeObserver(observer)
-    }
-    @Suppress("UNCHECKED_CAST")
-    return data as T
-}
-
-
-class StoryPagingSource : PagingSource<Int, LiveData<List<StoryResponseData>>>() {
-    companion object {
-        fun snapshot(items: List<StoryResponseData>): PagingData<StoryResponseData> {
-            return PagingData.from(items)
-        }
-    }
-    override fun getRefreshKey(state: PagingState<Int, LiveData<List<StoryResponseData>>>): Int {
-        return 0
-    }
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LiveData<List<StoryResponseData>>> {
-        return LoadResult.Page(emptyList(), 0, 1)
-    }
-}
-
 val noopListUpdateCallback = object : ListUpdateCallback {
     override fun onInserted(position: Int, count: Int) {}
     override fun onRemoved(position: Int, count: Int) {}
     override fun onMoved(fromPosition: Int, toPosition: Int) {}
     override fun onChanged(position: Int, count: Int, payload: Any?) {}
 }
-class DiffFavoriteEventCallback : DiffUtil.ItemCallback<StoryResponseData>() {
-    override fun areItemsTheSame(
-        oldItem: StoryResponseData,
-        newItem: StoryResponseData
-    ): Boolean {
-        return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(
-        oldItem: StoryResponseData,
-        newItem: StoryResponseData
-    ): Boolean {
-        return oldItem == newItem
-    }
-}
-
-
-//class TestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-//    val name: TextView = view.findViewById(R.id.story_name)
-//    val description: TextView = view.findViewById(R.id.story_description)
-//    val photo: ImageView = view.findViewById(R.id.story_photo)
-//}
-
-
-//class TestAdapter : PagingDataAdapter<StoryResponseData, RecyclerView.ViewHolder>(DIFF_CALLBACK) {
-//    companion object {
-//        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<StoryResponseData>() {
-//            override fun areItemsTheSame(oldItem: StoryResponseData, newItem: StoryResponseData): Boolean {
-//                return oldItem.id == newItem.id
-//            }
-//
-//            override fun areContentsTheSame(oldItem: StoryResponseData, newItem: StoryResponseData): Boolean {
-//                return oldItem == newItem
-//            }
-//        }
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-//        // Implement the view holder creation
-//        val view = LayoutInflater.from(parent.context)
-//            .inflate(R.layout.story_item, parent, false)
-//        return TestViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//        // Implement the view binding
-//        if (holder is TestViewHolder) {
-//            val currentItem = getItem(position) ?: return
-//            holder.name.text = currentItem.name
-//            holder.description.text = currentItem.description
-//
-//            Glide.with(holder.photo.context)
-//                .load(currentItem.photoUrl)
-//                .placeholder(R.drawable.ic_launcher_background)
-//                .error(R.drawable.ic_launcher_background)
-//                .into(holder.photo)
-//
-//            holder.itemView.setOnClickListener {
-//                val user = StoryResponseData(
-//                    currentItem.id,
-//                    currentItem.name,
-//                    currentItem.description,
-//                    currentItem.photoUrl,
-//                    currentItem.createdAt,
-//                    currentItem.lat,
-//                    currentItem.lon
-//                )
-//                val intent = Intent(holder.itemView.context, DetailStory::class.java)
-//                intent.putExtra("storyDetail", user)
-//                holder.itemView.context.startActivity(intent)
-//            }
-//
-//            val fadeIn = AlphaAnimation(0.0f, 1.0f)
-//            fadeIn.duration = 500
-//            holder.itemView.startAnimation(fadeIn)
-//        }
-//    }
-//}
-//
-//class FakeLifecycleOwner : LifecycleOwner {
-//    private val lifecycleRegistry = LifecycleRegistry(this)
-//
-//    init {
-//        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-//        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-//        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-//    }
-//
-//    override val lifecycle: Lifecycle
-//        get() = lifecycleRegistry
-//}
